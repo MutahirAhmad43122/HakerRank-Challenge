428cd9ad56f4cb4036515518eeaddb1bbbb6744a
Mutahir <bscs22134@itu.edu.pk>
2024-06-25 03:46:38 -0700

    Updated Function

--- comparetriplets.cpp
+++ comparetriplets.cpp
@@ -1,4 +1,22 @@
 vector<int> compareTriplets(vector<int> a, vector<int> b) 
 {
-   
+    int AScore=0;
+    int BScore=0;
+    
+    for(int i=0;i<3;i++)
+    {
+        if(a[i]<b[i])
+        {
+            BScore=BScore+1;
+        }
+        
+        else if(a[i]>b[i])
+        {
+            AScore=AScore+1;
+        }
+        
+    }
+    
+    vector<int> res={AScore,BScore};
+    return res;
 }
