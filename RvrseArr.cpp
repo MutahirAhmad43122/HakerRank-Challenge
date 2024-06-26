5d674532f986034b76d53d942f036e72ccf440f9
Mutahir <bscs22134@itu.edu.pk>
2024-06-26 02:34:16 -0700

    Updated Function

--- RvrseArr.cpp
+++ RvrseArr.cpp
@@ -1,4 +1,11 @@
 vector<int> reverseArray(vector<int> a) {
-
+    int n=a.size();
+    vector<int> temp(n);
+    
+    for(int i=0;i<n;i++)
+    {
+        temp[i]=a[n-i-1];
+    }
+    return temp;
 
 }
